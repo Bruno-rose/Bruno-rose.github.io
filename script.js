aa379440470a5385247bc6f6a7aa3cacf8f3c1f9
Bruno <bruno@fintual.com>
2024-09-11 22:56:44 -0300

    feat: initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleHidden() {
+    var content = document.getElementById('hiddenContent');
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+    } else {
+        content.style.display = 'block';
+    }
+}
