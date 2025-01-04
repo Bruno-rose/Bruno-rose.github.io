33071ca09ac61bc6de33b75a810a0a837d24499d
Bruno <bruno.rodriguez.sep@gmail.com>
2025-01-04 00:21:43 -0300

    feat: change tempate

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleHidden() {
-    var content = document.getElementById('hiddenContent');
-    if (content.style.display === 'block') {
-        content.style.display = 'none';
-    } else {
-        content.style.display = 'block';
-    }
-}
